a29a943a683698d52acfa15eeb76d4896aa142f1
Nguyen Hoang Phat <88542260+hoangphat120805@users.noreply.github.com>
2026-03-05 15:29:43 +0700

    feat: Add author, tag, status, and genre validation in book creation

--- app/api/v1/book.py
+++ app/api/v1/book.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, status
 from app.api.deps import SessionDep, CurrentAdmin
 
-from app.crud import book as crud_book
+from app.crud import book as crud_book, tag as crud_tag
 from app.schema.auth import Message
 from app.schema.book import BookCreate, BookPublic, BookUpdate, BookRegister
 from app.schema.response import Response, ResponseList
 
 from app.utilities.exceptions.http.exc_400 import http_exc_400_bad_book_request
-from app.utilities.exceptions.http.exc_404 import http_exc_404_book_not_found_request
+from app.utilities.exceptions.http.exc_404 import (
+    http_exc_404_book_not_found_request, 
+    http_exc_404_author_not_found_request,
+    http_exc_404_genre_not_found, 
+    http_exc_404_status_not_found_request, 
+    http_exc_404_tag_not_found_request
+)
 from app.utilities.exceptions.http.exc_403 import http_exc_403_forbidden_request
 
 router = APIRouter(prefix="/books", tags=["book"])
@@ -17,6 +23,22 @@ async def create_book(session: SessionDep, current_admin: CurrentAdmin, book_reg
     existing_book = await crud_book.get_book_by_slug(session, book_register.slug)
     if existing_book:
         raise http_exc_400_bad_book_request(slug=book_register.slug)
+    # check if the author does not exist
+    author = await crud_book.get_author_by_id(session, book_register.author_id)
+    if not author:
+        raise http_exc_404_author_not_found_request(string=book_register.author_id)
+    # check if the tag does not exist
+    tag = await crud_tag.get_tag_by_id(session, book_register.tag_id)
+    if not tag:
+        raise http_exc_404_tag_not_found_request(string=book_register.tag_id)
+    # check if the book_status doen not exist
+    book_status = await crud_book.get_book_status_by_id(session, book_register.book_status_id)
+    if not book_status:
+        raise http_exc_404_status_not_found_request(string=book_register.book_status_id)
+    # check if the genre does not exist
+    genre = await crud_book.get_genre_by_id(session, book_register.genre_id)
+    if not genre:
+        raise http_exc_404_genre_not_found(genre_id=book_register.genre_id)
     book_in = BookCreate.model_validate(book_register, update={"creator_id": str(current_admin.id)})
     db_book = await crud_book.create_book(session, book_in)
     return Response(status_code=201, success=True, message="Book created successfully", data=db_book)
